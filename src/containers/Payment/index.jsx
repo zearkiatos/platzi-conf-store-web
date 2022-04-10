75c991639037fdf77fb4e93267e16a5ed67f9dc1
Pedro Capriles <pedrocapriles@Pedros-MacBook-Pro.local>
2022-04-10 12:26:43 -0400

    Building and styles payment flow

--- src/containers/Payment/index.jsx
+++ src/containers/Payment/index.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import '@styles/payment.css';
 
 const Payment = () => {
-    return <h1>Payment</h1>
+    return (
+        <div className="payment">
+            <div className="payment-content">
+                <h3>
+                    Order Resumen:
+                </h3>
+                <button type="button" className="payment-button">
+                    Payment with Paypal
+                </button>
+            </div>
+        </div>
+    )
 };
 
 export default Payment;
--- src/containers/Success/index.jsx
+++ src/containers/Success/index.jsx
@@ -1,9 +1,21 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import '@styles/success.css';
 
 const Success = () => {
-    return <h1>Success</h1>
+    return (
+        <div className="success">
+            <div className="success-content">
+                <h2>Pedro, Thank you for your buying</h2>
+                <span>Your order will arrive in 3 days to your address</span>
+                <div className="success-map">
+                    Google Maps
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Success;
