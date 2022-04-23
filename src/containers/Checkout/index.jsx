9c16fb2ea5cc8a7119473d1c7a43d2e96b343e34
zearkiatos <caprilespe@outlook.com>
2022-04-23 08:03:27 -0400

    Integrating react helmet for improvement the SEO with meta tags

--- src/containers/Checkout/index.jsx
+++ src/containers/Checkout/index.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/jsx-fragments */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppContext from '@context/AppContext';
 import { sumTotal } from '@utils';
 import '@styles/checkout.css';
@@ -39,13 +41,41 @@ const Checkout = () => {
     </aside>
   );
   return (
-    <div className="checkout">
-      <div className="checkout-content">
-        <h3>Order list</h3>
-        {renderCartList}
+    <Fragment>
+      <Helmet>
+        <title> Platzi Conf Store - Checkout </title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@zearkiatos" />
+        <meta name="twitter:creator" content="@zearkiatos" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://github.com/zearkiatos.png?size=200"
+        />
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://github.com/zearkiatos.png?size=200"
+        />
+        <meta
+          property="og:url"
+          content="https://platzi-conf-store-web.firebaseapp.com/"
+        />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <div className="checkout">
+        <div className="checkout-content">
+          <h3>Order list</h3>
+          {renderCartList}
+        </div>
+        {renderTotal}
       </div>
-      {renderTotal}
-    </div>
+    </Fragment>
   );
 };
 
