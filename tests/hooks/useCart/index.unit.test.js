655cd710927eb2465508b1195b307d483ed15539
zearkiatos <caprilespe@outlook.com>
2022-05-04 19:44:13 -0400

    [Work in Progress] Add the custom hooks tests

---
+++ tests/hooks/useCart/index.unit.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-undef */
+import useCart from '@hooks/useCart';
+import ProductBuilder from '../../builders/productBuilder';
+
+describe('Suite unit test for useCart custom hook', () => {
+    test('Should add a new product to the cart', () => {
+            const payload = new ProductBuilder.build();
+            const {  }
+    });
+});
